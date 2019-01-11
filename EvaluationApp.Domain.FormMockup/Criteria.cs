9adec8416db5ca84b8a468975f42f2ac4a6e4bc1
mihaitoropu <toropumihai@gmail.com>
2019-01-11 22:11:39 +0200

    updated form mockup model

---
+++ EvaluationApp.Domain.FormMockup/Criteria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvaluationApp.Domain.FormMockup
+{
+    public class Criteria
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public int CreatedBy { get; set; }
+        public int ModifiedBy { get; set; }
+    }
+}
